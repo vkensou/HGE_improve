3ed03a894206be244bc8d75240e58d64816eb11c
vvsxr <vvsxrwan@163.com>
2014-03-04 16:27:56 +0800

    修改hgeBox2dDraw，在创建新的hgeBox2dDraw之后调用SetTransform()来设置box2d和hge之间的额变换，以使b2Draw在正确的坐标下绘制

--- include/hgeBox2dDraw.h
+++ include/hgeBox2dDraw.h
@@ -11,7 +11,7 @@ class hgeBox2dDraw :
 private:
 	HGE* hge;
 public:
-	hgeBox2dDraw(){hge=hgeCreate(HGE_VERSION);};
+	hgeBox2dDraw(){hge=hgeCreate(HGE_VERSION);_dx = _dy =0.f ;_hs = _vs =1.f;};
 	~hgeBox2dDraw(){hge->Release();};
 
 	virtual void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
@@ -25,4 +25,12 @@ public:
 	virtual void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
 
 	virtual void DrawTransform(const b2Transform& xf);
+
+	void SetTransform(float TransX,float TransY,float HScale,float VScale);
+private:
+	float _dx,_dy;
+	float _hs,_vs;
+	float tx(float x){return _dx + x * _hs;}
+	float ty(float y){return _dy + y * _vs;}
 };
+
--- src/helpers/hgeBox2dDraw.cpp
+++ src/helpers/hgeBox2dDraw.cpp
@@ -4,27 +4,20 @@ void hgeBox2dDraw::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const 
 {
 	if(!hge) return;
 
-	float adjustW = hge->System_GetState(HGE_SCREENWIDTH)/2.0f;
-	float adjustH = hge->System_GetState(HGE_SCREENHEIGHT)/2.0f;
-
 	for(int i=0;i<vertexCount-1;i++)
-		hge->Gfx_RenderLine(adjustW + vertices[i].x, adjustH - vertices[i].y, adjustW + vertices[i+1].x, adjustH - vertices[i+1].y, ARGB(255,color.r*255,color.g*255,color.b*255));
+		hge->Gfx_RenderLine(tx(vertices[i].x), ty(vertices[i].y), tx(vertices[i+1].x), ty(vertices[i+1].y), ARGB(255,color.r*255,color.g*255,color.b*255));
 
-	hge->Gfx_RenderLine(adjustW + vertices[vertexCount-1].x, adjustH - vertices[vertexCount-1].y, adjustW + vertices[0].x, adjustH - vertices[0].y, ARGB(255,color.r*255,color.g*255,color.b*255));
-
+	hge->Gfx_RenderLine(tx(vertices[vertexCount-1].x), ty(vertices[vertexCount-1].y), tx(vertices[0].x), ty(vertices[0].y), ARGB(255,color.r*255,color.g*255,color.b*255));
 }
 
 void hgeBox2dDraw::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
 {
 	if(!hge) return;
 
-	float adjustW = hge->System_GetState(HGE_SCREENWIDTH)/2.0f;
-	float adjustH = hge->System_GetState(HGE_SCREENHEIGHT)/2.0f;
-
 	for(int i=0;i<vertexCount-1;i++)
-		hge->Gfx_RenderLine(adjustW + vertices[i].x, adjustH - vertices[i].y, adjustW + vertices[i+1].x, adjustH - vertices[i+1].y, ARGB(255,color.r*255,color.g*255,color.b*255));
+		hge->Gfx_RenderLine(tx(vertices[i].x), ty(vertices[i].y), tx(vertices[i+1].x), ty(vertices[i+1].y), ARGB(255,color.r*255,color.g*255,color.b*255));
 
-	hge->Gfx_RenderLine(adjustW + vertices[vertexCount-1].x, adjustH - vertices[vertexCount-1].y, adjustW + vertices[0].x, adjustH - vertices[0].y, ARGB(255,color.r*255,color.g*255,color.b*255));
+	hge->Gfx_RenderLine(tx(vertices[vertexCount-1].x), ty(vertices[vertexCount-1].y), tx(vertices[0].x), ty(vertices[0].y), ARGB(255,color.r*255,color.g*255,color.b*255));
 }
 
 void hgeBox2dDraw::DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color)
@@ -34,7 +27,7 @@ void hgeBox2dDraw::DrawCircle(const b2Vec2& center, float32 radius, const b2Colo
 	float r = M_2PI/10;
 
 	for(int i = 0;i<10;i++)
-		hge->Gfx_RenderLine(center.x + radius * cos(r * i) ,center.y + radius * sin(r * i), center.x + radius * cos(r * i + r) ,center.y + radius * sin(r * i + r), ARGB(255,color.r*255,color.g*255,color.b*255));
+		hge->Gfx_RenderLine(tx(center.x + radius * cos(r * i)) ,ty(center.y + radius * sin(r * i)), tx(center.x + radius * cos(r * i + r)) ,ty(center.y + radius * sin(r * i + r)), ARGB(255,color.r*255,color.g*255,color.b*255));
 }
 
 void hgeBox2dDraw::DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color)
@@ -44,7 +37,7 @@ void hgeBox2dDraw::DrawSolidCircle(const b2Vec2& center, float32 radius, const b
 	float r = M_2PI/10;
 
 	for(int i = 0;i<10;i++)
-		hge->Gfx_RenderLine(center.x + radius * cos(r * i) ,center.y + radius * sin(r * i), center.x + radius * cos(r * i + r) ,center.y + radius * sin(r * i + r), ARGB(255,color.r*255,color.g*255,color.b*255));
+		hge->Gfx_RenderLine(tx(center.x + radius * cos(r * i)) ,ty(center.y + radius * sin(r * i)), tx(center.x + radius * cos(r * i + r)) ,ty(center.y + radius * sin(r * i + r)), ARGB(255,color.r*255,color.g*255,color.b*255));
 }
 
 void hgeBox2dDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color)
@@ -54,7 +47,7 @@ void hgeBox2dDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color
 	float adjustW = hge->System_GetState(HGE_SCREENWIDTH)/2.0f;
 	float adjustH = hge->System_GetState(HGE_SCREENHEIGHT)/2.0f;
 
-	hge->Gfx_RenderLine(adjustW + p1.x, adjustH - p1.y, adjustW + p2.x, adjustH - p2.y, ARGB(255,color.r*255,color.g*255,color.b*255));
+	hge->Gfx_RenderLine(tx(p1.x), ty(p1.y), tx(p2.x), ty(p2.y), ARGB(255,color.r*255,color.g*255,color.b*255));
 }
 
 void hgeBox2dDraw::DrawTransform(const b2Transform& xf)
@@ -73,4 +66,10 @@ void hgeBox2dDraw::DrawTransform(const b2Transform& xf)
 	//p2 = p1 + k_axisScale * xf.R.col2;
 	//hge->Gfx_RenderLine(adjustW + p1.x, adjustH - p1.y, adjustW + p2.x, adjustH - p2.y, ARGB(255,0,255,0));
 
+}
+
+void hgeBox2dDraw::SetTransform(float TransX,float TransY,float HScale,float VScale)
+{
+	_dx = TransX;_dy = TransY;
+	_hs = HScale;_vs = VScale;
 }
